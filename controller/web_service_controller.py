2927cbff457829e131b3027e033eacb5f332236e
tymian-sunny <tymian.sunny@gmail.com>
2025-05-02 21:38:46 +0800

    添加了缓存处理逻辑

--- controller/web_service_controller.py
+++ controller/web_service_controller.py
@@ -1,4 +1,6 @@
-from flask import Flask, Response, request
+from functools import wraps
+
+from flask import Flask, Response, request,make_response
 import time
 from flask_cors import CORS
 
@@ -6,6 +8,18 @@ from controller.SearchController import SearchController
 
 app = Flask(__name__)
 CORS(app)
+
+# 装饰器封装的缓存处理逻辑
+def cache_control(max_age=3600):
+    def decorator(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            resp = make_response(f(*args, **kwargs))
+            resp.headers['Cache-Control'] = f'public, max-age={max_age}'
+            return resp
+        return wrapped
+    return decorator
+
 @app.route('/test')
 def stream():
     def generate():
@@ -16,10 +30,12 @@ def stream():
     return Response(generate(), mimetype='text/event-stream')
 
 @app.route('/getAnimePlayUrl',methods = ['GET'])
+@cache_control(max_age=600)
 def getAnimePlayUrl():
     name = request.args.get('name')
     searchController = SearchController()
     return Response(searchController.get_anime(name), mimetype='text/event-stream')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
